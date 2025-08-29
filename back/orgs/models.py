7132c9e96c7d56642ebc62da879fe110e5aa4392
gal.esm49 <esm.2001@mail.ru>
2025-08-29 23:12:03 +0500

    Update models.py

--- back/orgs/models.py
+++ back/orgs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class License(models.Model):
+    code = models.CharField(max_length=64, unique=True)
+    issued_at = models.DateField(null=True, blank=True)
+    def __str__(self): return self.code
+
+class FinancialOrg(models.Model):
+    name = models.CharField(max_length=255)
+    bin = models.CharField(max_length=12, unique=True)
+    logo = models.ImageField(upload_to="logos/", blank=True, null=True)
+    licenses = models.ManyToManyField(License, blank=True)
+    def __str__(self): return f"{self.name} ({self.bin})"
+
