d88edc32ba5fdfdc59f00e78589320a0d5b5927e
gal.esm49 <esm.2001@mail.ru>
2025-08-29 23:14:35 +0500

    Update views.py

--- back/orgs/views.py
+++ back/orgs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import FinancialOrg, License
+from .serializers import FinancialOrgSerializer, LicenseSerializer
+from .permissions import IsAdminOrReadOnly
 
-# Create your views here.
+class FinancialOrgViewSet(ModelViewSet):
+    queryset = FinancialOrg.objects.all().order_by("name")
+    serializer_class = FinancialOrgSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    filterset_fields = ["bin","name"]
+
+class LicenseViewSet(ModelViewSet):
+    queryset = License.objects.all().order_by("code")
+    serializer_class = LicenseSerializer
+    permission_classes = [IsAdminOrReadOnly]
