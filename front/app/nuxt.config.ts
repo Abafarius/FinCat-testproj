cbf788789def8f70392cb4b91e68a7799f97d0d2
gal.esm49 <esm.2001@mail.ru>
2025-08-30 13:04:53 +0500

    Update nuxt.config.ts

--- front/app/nuxt.config.ts
+++ front/app/nuxt.config.ts
@@ -1,5 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://127.0.0.1:8000/api",
+    },
+  },
+  app: {
+    head: {
+      title: "Fin Catalog",
+      meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+    },
+  },
 })
+
+
