8186feb09d5da75fd541fa09af30b842d650f004
gal.esm49 <esm.2001@mail.ru>
2025-08-30 12:21:42 +0500

    Add BIN format validator to FinancialOrg model
    Introduces a RegexValidator to ensure the 'bin' field in the FinancialOrg model is exactly 12 digits. This enforces stricter data integrity for organization BINs.

--- back/orgs/models.py
+++ back/orgs/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
+
+bin_validator = RegexValidator(regex=r"^\d{12}$", message="BIN must be exactly 12 digits")
 
 class License(models.Model):
     code = models.CharField(max_length=64, unique=True)
@@ -7,7 +11,7 @@ class License(models.Model):
 
 class FinancialOrg(models.Model):
     name = models.CharField(max_length=255)
-    bin = models.CharField(max_length=12, unique=True)
+    bin = models.CharField(max_length=12, unique=True, validators=[bin_validator])
     logo = models.ImageField(upload_to="logos/", blank=True, null=True)
     licenses = models.ManyToManyField(License, blank=True)
     def __str__(self): return f"{self.name} ({self.bin})"
