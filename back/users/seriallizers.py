2ab0018080b0c6053fb6eaba25130fafcc45b929
gal.esm49 <esm.2001@mail.ru>
2025-08-30 12:19:10 +0500

    Add RegisterSerializer for user registration
    Introduces a RegisterSerializer class using Django REST Framework to handle user registration, including password validation and user creation with hashed passwords.

---
+++ back/users/seriallizers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ("username", "password", "email", "first_name", "last_name")
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data["username"],
+            email=validated_data.get("email", ""),
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", ""),
+            is_active=True,
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
